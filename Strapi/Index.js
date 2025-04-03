672264603197a23a497c0520c1e70fca00fc2164
AFS0027161 <AFS0027161@agderskolen.no>
2025-04-03 09:25:53 +0200

    Added Dikt.html and updated styles for better appearance

--- Strapi/Index.js
+++ Strapi/Index.js
@@ -1,9 +1,9 @@
 const API_URL = 'https://complete-health-85c8d30342.strapiapp.com';
 const API_KEY = '4c9f869b9d3a2c03992f1c5b89c223ea4c6cea2f1fcb09302074c4f905aeaf4fc5d3c72e9fad3916c98f2ee82f5aa55838c94fc4a92b9e10ba822064a92d8fad7eb8af5a7f7df4392fe4c660e6b9391d3f566fb1653bbc30c520979e5f6ab13ace980cef0f2e96be898f4cf6159cba4bc4a882a6ed1bb8a47ae0c87306e632bb';
 
-async function fetchList() {
-    console.log("fetchList");
-    const resp = await fetch(API_URL + '/api/nyheter?populate=*', {
+async function fetchList(contentType) {
+    console.log(`fetchList for ${contentType}`);
+    const resp = await fetch(API_URL + `/api/${contentType}?populate=*`, {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + API_KEY
@@ -11,7 +11,11 @@ async function fetchList() {
     });
     const response = await resp.json();
     for (let doc of response.data) {
-        await showDocument(doc);
+        if (contentType === 'nyheter') {
+            await showDocument(doc);
+        } else if (contentType === 'dikts') {
+            await showDikts(doc);
+        }
     }
 }
 
@@ -26,5 +30,16 @@ async function showDocument(doc) {
     `;
 }
 
-// Henter nyheter ved lasting av siden
-window.onload = () => fetchList();
+async function showDikts(doc) {
+    document.querySelector('#Dikt').innerHTML += `
+        <h2>${(doc.Tittel)}</h2>
+        <pre>${doc.Tekst}</pre>
+        <p>${doc.Forfatter}</p>
+    `;
+}
+
+// Henter nyheter og dikt ved lasting av siden
+window.onload = () => {
+    fetchList('nyheter');
+    fetchList('dikts');
+};
